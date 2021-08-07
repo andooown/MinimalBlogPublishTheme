90cc3a8d3f103178e90bdb537299cdaf16dff355
andooown <andooown.dev@gmail.com>
2021-08-08 02:19:03 +0900

    Add dependency on swift-collections

--- Package.swift
+++ Package.swift
@@ -10,13 +10,15 @@ let package = Package(
             targets: ["MinimalBlogPublishTheme"]),
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0")
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "0.0.1"),
     ],
     targets: [
         .target(
             name: "MinimalBlogPublishTheme",
             dependencies: [
-                "Publish"
+                "Publish",
+                .product(name: "Collections", package: "swift-collections"),
             ]),
         .testTarget(
             name: "MinimalBlogPublishThemeTests",
