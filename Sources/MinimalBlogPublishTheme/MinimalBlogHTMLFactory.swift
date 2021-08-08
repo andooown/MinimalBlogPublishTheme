d9a51c7e1ad38783e4e306dfffe6777a4413aead
andooown <andooown.dev@gmail.com>
2021-08-09 02:21:55 +0900

    Add Bootstrap v5.1

--- Sources/MinimalBlogPublishTheme/MinimalBlogHTMLFactory.swift
+++ Sources/MinimalBlogPublishTheme/MinimalBlogHTMLFactory.swift
@@ -21,9 +21,11 @@ extension MinimalBlogHTMLFactory {
 
         return HTML(head: [
             .title("TITLE"),
-            .encoding(.utf8)
+            .encoding(.utf8),
+            .link(.href(URL(string: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css")!), .rel(.stylesheet), .integrity("sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"), .attribute(named: "crossorigin", value: "anonymous"))
         ]) {
             ItemList(divideItems(context.sections[primarySection].items))
+            Node<HTML.BodyContext>.script(.src(URL(string: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js")!), .integrity("sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"), .attribute(named: "crossorigin", value: "anonymous"))
         }
     }
 
@@ -33,9 +35,11 @@ extension MinimalBlogHTMLFactory {
 
         return HTML(head: [
             .title("TITLE"),
-            .encoding(.utf8)
+            .encoding(.utf8),
+            .link(.href(URL(string: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css")!), .rel(.stylesheet), .integrity("sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"), .attribute(named: "crossorigin", value: "anonymous"))
         ]) {
             ItemList(divideItems(section.items))
+            Node<HTML.BodyContext>.script(.src(URL(string: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js")!), .integrity("sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"), .attribute(named: "crossorigin", value: "anonymous"))
         }
     }
 
